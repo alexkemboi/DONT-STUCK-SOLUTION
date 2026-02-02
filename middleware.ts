aaf41ef41349ce2fefcfe9773c4cf740cc64d131
akemboi <alexkipchirchirembo@gmail.com>
2026-02-02 22:37:24 +0300

    added middleware ts

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("auth-token");
+
+  if (!token && request.nextUrl.pathname.startsWith("/")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
