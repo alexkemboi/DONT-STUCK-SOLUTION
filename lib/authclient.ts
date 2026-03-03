1419c616433c5e12658beb3ed815e48210299b53
kibochamark <regexmark@gmail.com>
2026-03-03 06:43:05 +0300

    fix: update base URL in auth client to use production server

--- lib/authclient.ts
+++ lib/authclient.ts
@@ -6,6 +6,6 @@ import { auth } from "./auth";
 export const authClient = createAuthClient({
     plugins: [customSessionClient<typeof auth>()],
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: "https://dont-stuck-solution.vercel.app"
-    baseURL: "http://localhost:3000"
+    baseURL: "https://dont-stuck-solution.vercel.app"
+    // baseURL: "http://localhost:3000"
 })
